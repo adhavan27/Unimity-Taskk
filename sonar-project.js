69fa3372053dcdc42783993b1663e119f16288c0
adhavan27 <105037638+adhavan27@users.noreply.github.com>
2022-09-29 22:46:05 +0530

    Create sonar-project.js

---
+++ sonar-project.js
@@ -0,0 +1,19 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://3.110.163.169:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJsMithunTechnologies',
+	    'sonar.login': 'add3697c804c4940a619a0692643a639f45a2dc8',
+	    //'sonar.login': 'admin',
+	    //'sonar.password': 'admin',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
